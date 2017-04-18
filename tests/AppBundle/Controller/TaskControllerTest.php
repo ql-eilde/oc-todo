09b43e8fad28696d726462bdedf8a9cb7757d898
Quentin L'eilde <me@quentinleilde.com>
2017-04-18 17:40:53 +0200

    add complete tests of Task and User controller

--- tests/AppBundle/Controller/TaskControllerTest.php
+++ tests/AppBundle/Controller/TaskControllerTest.php
@@ -3,18 +3,93 @@
 namespace Tests\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Faker\Factory;
 
 class TaskControllerTest extends WebTestCase
 {
+    /**
+     * @var \Doctrine\ORM\EntityManager
+     */
+    private $em;
+
+    private $client;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp()
+    {
+        self::bootKernel();
+
+        $this->em = static::$kernel->getContainer()
+            ->get('doctrine')
+            ->getManager();
+
+        $this->client = static::createClient(array(), array(
+            'PHP_AUTH_USER' => 'qleilde',
+            'PHP_AUTH_PW' => 'qleilde',
+        ));
+    }
+
     public function testList()
     {
-	    $client = static::createClient(array(), array(
-		    'PHP_AUTH_USER' => 'qleilde',
-			'PHP_AUTH_PW' => 'qleilde',
-		));
+		$this->client->request('GET', '/tasks');
 
-		$client->request('GET', '/tasks');
+		$this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+    }
 
-		$this->assertEquals(200, $client->getResponse()->getStatusCode());
+    public function testCreate()
+    {
+        $crawler = $this->client->request('GET', '/tasks/create');
+
+        $form = $crawler->selectButton('Ajouter')->form();
+
+        $faker = Factory::create();
+
+        $form['task[title]'] = $faker->sentence();
+        $form['task[content]'] = $faker->text();
+
+        $this->client->submit($form);
+
+        $this->assertTrue($this->client->getResponse()->isRedirect());
+    }
+
+    public function testEdit()
+    {
+        $tasks = $this->em->getRepository('AppBundle:Task')->findAll();
+        $task = end($tasks);
+
+        $crawler = $this->client->request('GET', '/tasks/'.$task->getId().'/edit');
+
+        $form = $crawler->selectButton('Modifier')->form();
+
+        $faker = Factory::create();
+
+        $form['task[title]'] = $faker->sentence();
+        $form['task[content]'] = $faker->text();
+
+        $this->client->submit($form);
+
+        $this->assertTrue($this->client->getResponse()->isRedirect());
+    }
+
+    public function testTaskToggle()
+    {
+        $tasks = $this->em->getRepository('AppBundle:Task')->findAll();
+        $task = end($tasks);
+
+        $this->client->request('GET', '/tasks/'.$task->getId().'/toggle');
+
+        $this->assertTrue($this->client->getResponse()->isRedirect());
+    }
+
+    public function testTaskDelete()
+    {
+        $tasks = $this->em->getRepository('AppBundle:Task')->findAll();
+        $task = end($tasks);
+
+        $this->client->request('GET', '/tasks/'.$task->getId().'/delete');
+
+        $this->assertTrue($this->client->getResponse()->isRedirect());
     }
 }
--- tests/AppBundle/Controller/UserControllerTest.php
+++ tests/AppBundle/Controller/UserControllerTest.php
@@ -3,18 +3,79 @@
 namespace Tests\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Faker\Factory;
 
 class UserControllerTest extends WebTestCase
 {
-    public function testList()
+    /**
+     * @var \Doctrine\ORM\EntityManager
+     */
+    private $em;
+
+    private $client;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp()
     {
-        $client = static::createClient(array(), array(
+        self::bootKernel();
+
+        $this->em = static::$kernel->getContainer()
+            ->get('doctrine')
+            ->getManager();
+
+        $this->client = static::createClient(array(), array(
             'PHP_AUTH_USER' => 'qleilde',
             'PHP_AUTH_PW' => 'qleilde',
         ));
+    }
 
-        $client->request('GET', '/users');
+    public function testList()
+    {
+        $this->client->request('GET', '/users');
 
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+    }
+
+    public function testCreate()
+    {
+        $crawler = $this->client->request('GET', '/users/create');
+
+        $form = $crawler->selectButton('Ajouter')->form();
+
+        $faker = Factory::create();
+        $pass = $faker->password;
+
+        $form['user[username]'] = $faker->userName;
+        $form['user[password][first]'] = $pass;
+        $form['user[password][second]'] = $pass;
+        $form['user[email]'] = $faker->email;
+
+        $this->client->submit($form);
+
+        $this->assertTrue($this->client->getResponse()->isRedirect());
+    }
+
+    public function testEdit()
+    {
+        $users = $this->em->getRepository('AppBundle:User')->findAll();
+        $user = end($users);
+
+        $crawler = $this->client->request('GET', '/users/'.$user->getId().'/edit');
+
+        $form = $crawler->selectButton('Modifier')->form();
+
+        $faker = Factory::create();
+        $pass = $faker->password;
+
+        $form['user[username]'] = $faker->userName;
+        $form['user[password][first]'] = $pass;
+        $form['user[password][second]'] = $pass;
+        $form['user[email]'] = $faker->email;
+
+        $this->client->submit($form);
+
+        $this->assertTrue($this->client->getResponse()->isRedirect());
     }
 }
